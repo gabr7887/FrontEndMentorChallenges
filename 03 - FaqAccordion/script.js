1bd868280a978530d886fd7aeb4f84823bb3b001
gabr7887 <luizgabriel7887@gmail.com>
2024-06-18 11:34:59 -0300

    finalizado 03- FaqAccordion

--- 03 - FaqAccordion/script.js
+++ 03 - FaqAccordion/script.js
@@ -2,6 +2,39 @@ const accordions = document.querySelectorAll('.accordion-item');
 
 accordions.forEach((item) =>
   item.addEventListener('click', () => {
-    item.classList.toggle('active');
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+      item.children[0].children[1].setAttribute(
+        'src',
+        './assets/images/icon-plus.svg',
+      );
+    } else {
+      item.classList.add('active');
+      item.children[0].children[1].setAttribute(
+        'src',
+        './assets/images/icon-minus.svg',
+      );
+    }
   }),
 );
+
+addEventListener('keyup', (event) => {
+  if (
+    event.key === 'Enter' &&
+    event.target.classList.contains('accordion-item')
+  ) {
+    if (event.target.classList.contains('active')) {
+      event.target.classList.remove('active');
+      event.target.children[0].children[1].setAttribute(
+        'src',
+        './assets/images/icon-plus.svg',
+      );
+    } else {
+      event.target.classList.add('active');
+      event.target.children[0].children[1].setAttribute(
+        'src',
+        './assets/images/icon-minus.svg',
+      );
+    }
+  }
+});
